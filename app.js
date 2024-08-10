c400da938cce374341390c95636d43adff9bdf5b
Knderson21 <knderson21@gmail.com>
2024-08-09 22:46:51 -0400

    more js

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ close.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', e => {
     e.target === modal ? modal.classList.remove('show-modal') : false;
 })
+
+let nameField = document.getElementById("name");
+let emailField = document.getElementById("email");
+let passwordField = document.getElementById("password");
+let password2Field = document.getElementById("password2");
+
+function confirmFilledOut(nameField, emailField, passwordField, password2Field) {
+    if (nameField.value === null) {
+        nameField.style.border = '1px solid red';
+    }
+}
